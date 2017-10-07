e376db552919dd82729e8b1be059ba5fe6b14e19
deeper <kaiyuan@qq.com>
2017-10-07 19:36:38 +0800

    add loginView

--- src/application/App.java
+++ src/application/App.java
@@ -1,11 +1,8 @@
 package application;
 
-import application.dialog.DialogHelper;
-import application.dialog.LayoutInflater;
+import application.view.LoginView;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -13,14 +10,7 @@ public class App extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			VBox vBox = LayoutInflater.inflate("activity_main", VBox.class);
-			Button button = new Button("button");
-			button.setOnAction(e->{
-				DialogHelper.about();
-			});
-
-			vBox.getChildren().add(button);
-			Scene scene = new Scene(vBox,500,500);
+			Scene scene = new Scene(new LoginView().getView());
 			scene.getStylesheets().add("css/application.css");
 
 			primaryStage.setTitle(Constants.APP_NAME);
---
+++ src/application/view/LoginView.java
@@ -0,0 +1,15 @@
+package application.view;
+
+import application.dialog.LayoutInflater;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginView implements View{
+
+	@Override
+	public Parent getView() {
+		AnchorPane anchorPane = LayoutInflater.inflate("login", AnchorPane.class);
+		return anchorPane;
+	}
+
+}
