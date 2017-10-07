98b7f00d901583cd7a3c50b7514ea993c5f2dd68
deeper <kaiyuan@qq.com>
2017-10-08 00:07:00 +0800

    update main_left

--- src/application/view/MainView.java
+++ src/application/view/MainView.java
@@ -6,9 +6,15 @@ import application.dialog.LayoutInflater;
 import javafx.scene.Parent;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
 public class MainView implements View {
+	private final ImageView rootIcon = new ImageView(new Image(getClass().getClassLoader().getResourceAsStream("images/blue/tree_root.png")));
+	private final ImageView oneIcon = new ImageView(new Image(getClass().getClassLoader().getResourceAsStream("images/blue/tree_article.png")));
+	private final ImageView twoIcon = new ImageView(new Image(getClass().getClassLoader().getResourceAsStream("images/blue/tree_category.png")));
+	private final ImageView threeIcon = new ImageView(new Image(getClass().getClassLoader().getResourceAsStream("images/blue/tree_setting.png")));
 
 	@Override
 	public Parent getView() {
@@ -17,19 +23,20 @@ public class MainView implements View {
 
 			TreeView treeView = new TreeView();
 			// TreeView的内容也是非Node类型，所以不能用SceneBuilder绘图。
-		    TreeItem<String> treeItemRoot = new TreeItem<String>("Root node");
+		    TreeItem<String> treeItemRoot = new TreeItem<String>("导航菜单",rootIcon);
 		    treeItemRoot.getChildren().addAll(Arrays.asList(
-	                new TreeItem<String>("第1季"),
-	                new TreeItem<String>("第2季"),
-	                new TreeItem<String>("第3季")));
+		    		new TreeItem<String>("文章管理",oneIcon),
+		    		new TreeItem<String>("类别管理",twoIcon),
+		    		new TreeItem<String>("系统设置",threeIcon)
+		    		));
 
 		    treeItemRoot.getChildren().get(2).getChildren().addAll(Arrays.asList(
-	                new TreeItem<String>("第3季-1"),
-	                new TreeItem<String>("第3季-1"),
-	                new TreeItem<String>("第3季-1")
+	                new TreeItem<String>("xx"),
+	                new TreeItem<String>("xx"),
+	                new TreeItem<String>("退出")
 	                ));
 
-		    treeItemRoot.setExpanded(true);
+		    treeItemRoot.setExpanded(false);
 
 	        treeView.setShowRoot(true);
 	        treeView.setRoot(treeItemRoot);
